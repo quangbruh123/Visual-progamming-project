48f3ad49795194c8f71d9929758085bceb9bbe60
Nguyen_Ba_Cong <20520417@gm.uit.edu.vn>
2021-11-15 12:47:17 +0700

    New UI

---
+++ Learning Sorting Algorithm/Learning Sorting Algorithm/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Learning_Sorting_Algorithm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static MainWindow instance;
+        public MainWindow()
+        {
+            InitializeComponent();
+            instance = this;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            WelcomeWindow f = new WelcomeWindow();
+            this.Hide();
+            f.ShowDialog();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnLogOut_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            WelcomeWindow.instance.Show();
+        }
+    }
+}
