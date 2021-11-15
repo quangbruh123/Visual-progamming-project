046be6649518c28898101d5dbd9ec647ca78c4fe
Wibuman <tgs.joker@gmail.com>
2021-11-15 22:11:37 +0700

    UI mới
     Tạo mới UI không dựa trên UI mới của TheStarGazer, sử dụng duy nhất 1 window để load các page (form) lên thay vì mở 1 window mới mỗi lần đổi form.
                Chưa nhất trí được phong cách cho UI

---
+++ Da projekt/Page2.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Da_projekt
+{
+    /// <summary>
+    /// Interaction logic for Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void ComparisionGroup_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Learn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Window1.instance.MainContentFrame.Content = new Page1();
+        }
+    }
+}
---
+++ Da projekt/Window1.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Da_projekt
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public static Window1 instance;
+
+
+        public Window1()
+        {
+            //Tắt Splashscreen
+            App.ss.Close(TimeSpan.FromSeconds(0.2));
+
+            InitializeComponent();
+            instance = this;
+
+            MainContentFrame.Content = new Page1();
+        }
+
+        private void Frame_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+
+        }
+    }
+}
