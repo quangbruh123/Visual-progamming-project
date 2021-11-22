b0a5ea5cf295a2d0b8355e26485148e079f89027
Hew01 <maiphuctam62@gmail.com>
2021-11-22 20:08:09 +0700

    add Merge + Heap

---
+++ Da projekt v0,1.5/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Da_projekt
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static SplashScreen ss;
+
+        App()
+        {
+            ss = new SplashScreen("CbGwZ31.jpg");
+            ss.Show(true);
+        }
+    }
+}
