86503c5cdff63d8715b39a33eb0def0f93b88eac
Hew01 <83998840+Hew01@users.noreply.github.com>
2021-12-04 17:53:42 +0700

    Added textbox for sorts except quick and merge

---
+++ Visual-progamming-project-DMT/Da projekt/InsertionSort.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace Da_projekt
+{
+    class InsertionSort : SortEngine
+    {
+        List<Item> items;
+        List<Todo> todos;
+        SortSimulation sm;
+        int sortOder = 1;
+        TextBox textBox;
+
+        public InsertionSort(SortSimulation sortsim, List<Item> refitem, ref List<Todo> reftodo, TextBox t)
+        {
+            items = refitem;
+            todos = reftodo;
+            sm = sortsim;
+            textBox = t;
+        }
+
+        //bắt buộc phải sử dụng LearnSortPanel.instance.refresh()
+        //thay vì sm.refresh() nếu sort bằng thread.
+        //ko cần thiết nếu ko dùng thread
+        //do được viết trước khi bỏ dùng thread
+        //nên LearnSortPanel.instance.refresh() nên chưa thay.
+        //xài cái nào cũng đc
+        public int SortAsMethod()
+        {
+            Stopwatch sw = new Stopwatch();
+            
+            todos.Add(new Todo("IntroInsert", textBox));
+            todos.Add(new Todo("Refresh"));
+            for (int i = 1; i < items.Count; i++)
+            {
+                //todos.Add(new Todo("FancyPause"));
+                int key = items[i].data;
+                int j;
+                int reset = -1;
+                todos.Add(new Todo("ChangeColor", i, Colors.Red));
+                todos.Add(new Todo("Starting", i, textBox)); // starting index i
+                //todos.Add(new Todo("FancyPause"));
+                todos.Add(new Todo("Refresh"));
+                //todos.Add(new Todo("ChangeColor", i, Colors.Green));
+                for (j = i - 1; j >= 0 && key * sortOder < items[j].data * sortOder; j--)
+                {
+                    //todos.Add(new Todo("ChangeColor", j + 1, Colors.Green));
+                    items[j + 1].data = items[j].data;
+                    
+                    todos.Add(new Todo("Switch", j, j + 1, textBox));// chỗ này switch nhưng không thông báo gì cả 
+                    todos.Add(new Todo("ChangeColor", j+1, Colors.Green));
+                    todos.Add(new Todo("ChangeColor", j, Colors.Red));
+                    todos.Add(new Todo("Refresh"));
+                    reset = j;
+                }
+
+                if (reset != -1)
+                    todos.Add(new Todo("ChangeColor", reset, Colors.Red));
+                else if (j < 0)
+                {
+                    todos.Add(new Todo("ChangeColor", 0, Colors.Red));
+                    reset = j + 1;
+                }
+                if (reset != -1)
+                    todos.Add(new Todo("AnnounceInsert", i, reset, textBox)); // thông báo đã chèn phần tử thứ i về đúng chỗ
+                else
+                    todos.Add(new Todo("NoChangeInsert", textBox));
+                todos.Add(new Todo("Refresh"));
+                //todos.Add(new Todo("FancyPause"));
+                items[j + 1].data = key;
+                //todos.Add(new Todo("Refresh"));
+                todos.Add(new Todo("ResetColor", i));
+                if (reset != -1)
+                    for (j = reset; j < i; j++)
+                    {
+                        todos.Add(new Todo("ResetColor", j));
+                    }
+                todos.Add(new Todo("Done", textBox));
+            }
+            
+            todos.Add(new Todo("Refresh"));
+            sw.Stop();
+            return ((int)sw.ElapsedMilliseconds);//trả về thời gian sort.
+        }
+        public void SortAsThread()
+        { }
+    }
+}
---
+++ Visual-progamming-project-DMT/Da projekt/InterchangeSort.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace Da_projekt
+{
+    class InterchangeSort : SortEngine
+    {
+        List<Item> items;
+        List<Todo> todos;
+        SortSimulation sm;
+        int sortOder = 1;
+        TextBox textBox;
+        public InterchangeSort(SortSimulation sortsim, List<Item> refitem, ref List<Todo> reftodo, TextBox t)
+        {
+            items = refitem;
+            todos = reftodo;
+            sm = sortsim;
+            textBox = t;
+        }
+
+        //bắt buộc phải sử dụng LearnSortPanel.instance.refresh()
+        //thay vì sm.refresh() nếu sort bằng thread.
+        //ko cần thiết nếu ko dùng thread
+        //do được viết trước khi bỏ dùng thread
+        //nên LearnSortPanel.instance.refresh() nên chưa thay.
+        //xài cái nào cũng đc
+        public int SortAsMethod()
+        {
+            Stopwatch sw = new Stopwatch();
+            todos.Add(new Todo("IntroInterchange", textBox));
+            todos.Add(new Todo("Refresh"));
+            for (int i = 0; i < items.Count - 1; i++)
+            { 
+                //todos.Add(new Todo("FancyPause"));
+                todos.Add(new Todo("ChangeColor", i, Colors.Red)); // starting index i
+                todos.Add(new Todo("Starting", i, textBox));
+                todos.Add(new Todo("Refresh"));
+                //todos.Add(new Todo("FancyPause"));
+                for (int j = i + 1; j < items.Count; j++) 
+                {
+                    todos.Add(new Todo("ChangeColor", j, Colors.Orchid)); // starting index j
+                    todos.Add(new Todo("StartingSub", j, textBox));
+                    todos.Add(new Todo("Refresh"));
+
+                    if (items[i].data * sortOder > items[j].data * sortOder)
+                    {
+                        todos.Add(new Todo("ChangeColor", i, Colors.Green));
+                        todos.Add(new Todo("ChangeColor", j, Colors.Green));                       
+                        todos.Add(new Todo("Switch", i, j, textBox)); // swap i j
+                        todos.Add(new Todo("Refresh"));
+                        todos.Add(new Todo("ChangeColor", i, Colors.Red));
+                        int Backup = items[i].data;
+                        items[i].data = items[j].data;
+                        items[j].data = Backup;
+                    }
+                    todos.Add(new Todo("ResetColor", j));
+                    //todos.Add(new Todo("Refresh"));
+                }
+                //todos.Add(new Todo("FancyPause"));
+                todos.Add(new Todo("ResetColor", i));
+                todos.Add(new Todo("DoneIC", i, textBox)); // xong vòng lặp của thứ i
+                todos.Add(new Todo("Refresh"));
+            }
+            todos.Add(new Todo("Done", textBox));
+            todos.Add(new Todo("Refresh"));
+            sw.Stop();
+            return ((int)sw.ElapsedMilliseconds);//trả về thời gian sort.
+        }
+        public void SortAsThread()
+        { }
+    }
+}
