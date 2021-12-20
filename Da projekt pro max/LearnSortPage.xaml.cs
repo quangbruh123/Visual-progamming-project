62d93eeec9cd2301c4f6db4f4619792369e26c80
Wibuman <tgs.joker@gmail.com>
2021-12-20 10:54:43 +0700

    CHức năng 3.
    Chức năng 1 + 3 hoàn chỉnh, cần add MergeSort.
    Các ông xem còn thiếu gì thì add giùm

---
+++ Da projekt pro max/LearnSortPage.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Da_projekt
+{
+    /// <summary>
+    /// Interaction logic for LearnSortPanel.xaml
+    /// </summary>
+    ///
+    public partial class LearnSortPage : Page
+    {
+        SortSimulation sm;
+        Random rand = new Random();
+        List<Item> items = new List<Item>();
+        public bool isStarted = false;
+        bool first = true;
+
+        public LearnSortPage(SortType st)
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < Window1.instance.exampleCount; i++)
+            {
+                Item item = new Item(rand.Next(10, 100));
+
+                items.Add(item);
+            }
+            switch (st)
+            {
+                case SortType.BubbleSort:
+                    PageLabel.Content = "Bubble Sort";
+                    sm = new SortSimulation(SortType.BubbleSort, MainCanvas, items, TextDisplay);
+                    break;
+                case SortType.InsertionSort:
+                    PageLabel.Content = "Insertion Sort";
+                    sm = new SortSimulation(SortType.InsertionSort, MainCanvas, items, TextDisplay);
+                    break;
+                case SortType.InterchangeSort:
+                    PageLabel.Content = "Interchange Sort";
+                    sm = new SortSimulation(SortType.InterchangeSort, MainCanvas, items, TextDisplay);
+                    break;
+                case SortType.MergeSort:
+                    PageLabel.Content = "Merge Sort";
+                    MessageBox.Show("Tính năng chưa sẵn sàng.");
+                    break;
+                case SortType.Quicksort:
+                    PageLabel.Content = "Quick Sort";
+                    sm = new SortSimulation(SortType.Quicksort, MainCanvas, items, TextDisplay);
+                    break;
+                case SortType.SelectionSort:
+                    PageLabel.Content = "Selection Sort";
+                    sm = new SortSimulation(SortType.SelectionSort, MainCanvas, items, TextDisplay);
+                    break;
+                default:
+                    return;
+            }
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            if (Start.Content.ToString() == "Hoàn tất")
+            {
+                return;
+            }    
+            if (isStarted == false)
+            {
+                Start.Content = "Bước tiếp ";
+                List<Item> k = new List<Item>();
+                sm.LearnSort();
+                sm.ManualReplay();
+                isStarted = true;
+            }
+            else
+            {
+                sm.Pause();
+            }
+        }
+
+        private void Return_Click(object sender, RoutedEventArgs e)
+        {
+            sm.Stop();
+            sm = null;
+            Window1.instance.MainContentFrame.Content = null;
+            Window1.instance.MainContentFrame.Navigate(new Uri("LearnSortMenu.xaml", UriKind.Relative));
+        }
+
+        private void MainCanvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (first)
+            {
+                sm.Initialize();
+                first = false;
+            }
+        }
+    }
+}
---
+++ Da projekt pro max/WelcomePage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace Da_projekt
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class WelcomePage : Page
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+            this.RenderSize = Window1.instance.MainContentFrame.RenderSize;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1.instance.MainContentFrame.Navigate(new Uri("MainMenuPage.xaml", UriKind.Relative));
+        }
+
+        private void output_Click(object sender, RoutedEventArgs e)
+        {
+            SettingWindow sw = new SettingWindow();
+            sw.Show();
+        }
+    }
+}
